8574c2caba5ad77688c0bfe35ec7082d933749b5
prooholic <peterardiarta11@gmail.com>
2021-01-28 17:36:48 +0000

    added compilation scripts

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export KERNELNAME="LoliKernel"
+
+export LOCALVERSION=1
+
+export KBUILD_BUILD_USER=Peter
+
+export KBUILD_BUILD_HOST=DroneCI
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred
+
+source helper
+
+gen_toolchain
+
+send_msg "Start building ${KERNELNAME} ${LOCALVERSION} for ${DEVICES}..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+
+	build ${i} -newcam
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "Build succesfully in $((DIFF / 60))m $((DIFF % 60))s"
+
